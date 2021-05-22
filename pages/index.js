37d8f6ac95599535c2bbd1d39c2eeda90708fa07
joshua.lawrence <joshua98lawrence@gmail.com>
2021-05-21 20:58:36 -0600

    new site

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,63 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Container,
+Row,
+Col,
+Navbar,
+Button } from "react-bootstrap"
+
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Joshua Lawrence</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <Container fluid style={{backgroundColor: "#111"}}>
+        <Row>
+        <Col md="6" className="d-flex flex-column justify-content-center align-items-center">
+                <div style={{color: "white"}}>
+                    <h1 style={{fontSize: "64px"}}>Joshua Lawrence</h1>
+                    <h3>Software Engineer</h3>
+                    <h5>Java, JavaScript, Spring, React, Next.js, AWS</h5>
+                    <div className="d-flex flex-row">
+                      <h2><a href="https://github.com/joshua-lawrence"><AiFillGithub /></a></h2>
+                      <h2><a href="https://www.linkedin.com/in/joshuaglawrence/"><AiFillLinkedin /></a></h2>
+                    </div>
+                </div>
+            </Col>
+          <Col>
+            <img style={{width: "60%", paddingTop: "50px"}} src="me (1).png">
+            </img>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row style={{margin: "100px 0px 100px 0px"}}>
+          <Col md="4">
+              <a href="https://www.sadiephoto.com/" style={{width: "100% !important"}}>
+                <img src="Capture.PNG" style={{ margin: "auto", width: "100%", border: "1px solid grey"}}>
+                </img>
+                <h5 className="text-center pt-3">Sadie Photo</h5>
+              </a>
+          </Col>
+          <Col md="4">
+              <a href="https://www.sadiephoto.com/" style={{width: "100%"}}>
+                <img src="Capture2.PNG" style={{margin: "auto",width: "100%", border: "1px solid grey"}}>
+                </img>
+                <h5 className="text-center pt-3">Decheque</h5>
+              </a>
+          </Col>
+          <Col md="4">
+              <a href="https://worldclassintegration.vercel.app/" style={{width: "100%"}}>
+                <img src="Capture3.PNG" style={{ margin: "auto", width: "100%", border: "1px solid grey"}}>
+                </img>
+                <h5 className="text-center pt-3">World Class Integration</h5>
+              </a>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
