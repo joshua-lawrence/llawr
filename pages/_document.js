0093caf3e6cef882f139a5db3b15551d6f1f7794
Joshua Lawrence <joshua98lawrence@gmail.com>
2022-10-30 20:57:55 -0600

    updated site

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
-  
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import { ServerStyleSheets } from "@material-ui/core/styles";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
 
 export default class MyDocument extends Document {
   render() {
@@ -12,7 +11,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700&display=swap"
           />
         </Head>
         <body>
@@ -63,6 +62,9 @@ MyDocument.getInitialProps = async (ctx) => {
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
   };
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import { Container,
-Row,
-Col,
-Navbar,
-Button } from "react-bootstrap"
+import Head from "next/head";
+import { Col, Container, Row } from "react-bootstrap";
 
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
@@ -14,59 +10,131 @@ export default function Home() {
         <title>Joshua Lawrence</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container fluid style={{backgroundColor: "#111"}}>
-        <Row>
-        <Col md="6" className="d-flex flex-column justify-content-center align-items-center">
-                <div style={{color: "white"}}>
-                    <h1 style={{fontSize: "64px"}}>Joshua Lawrence</h1>
-                    <h3>Software Engineer</h3>
-                    <h5>Java, JavaScript, Spring, React, Next.js, AWS</h5>
-                    <div className="d-flex flex-row">
-                      <h2><a href="https://github.com/joshua-lawrence"><AiFillGithub /></a></h2>
-                      <h2><a href="https://www.linkedin.com/in/joshuaglawrence/"><AiFillLinkedin /></a></h2>
-                    </div>
+      <Container fluid style={{ backgroundColor: "#111" }}>
+        <Container style={{ backgroundColor: "#111" }}>
+          <Row>
+            <Col
+              md="7"
+              className="heading d-flex flex-column justify-content-center align-items-center"
+            >
+              <div style={{ color: "white" }}>
+                <h1 className="gradient-h1">Joshua Lawrence</h1>
+                <h3 style={{ fontWeight: 300, fontSize: "28px" }}>
+                  CTO at Velvet
+                </h3>
+                <h5 style={{ fontWeight: 100 }}>
+                  Product Design · Engineering · Data Driven Decision Making
+                </h5>
+                <div className="d-flex flex-row">
+                  <h2>
+                    <a href="https://github.com/joshua-lawrence">
+                      <AiFillGithub />
+                    </a>
+                  </h2>
+                  <h2>
+                    <a href="https://www.linkedin.com/in/joshuaglawrence/">
+                      <AiFillLinkedin />
+                    </a>
+                  </h2>
                 </div>
+              </div>
             </Col>
-          <Col>
-            <img style={{width: "60%", paddingTop: "50px"}} src="me (1).png">
-            </img>
-          </Col>
-        </Row>
+            <Col>
+              <img
+                className="me"
+                style={{
+                  width: "70%",
+                  paddingTop: "50px",
+                  marginLeft: "100px",
+                }}
+                src="me (1).png"
+              ></img>
+            </Col>
+          </Row>
+        </Container>
       </Container>
       <Container>
-        <Row style={{margin: "100px 0px 50px 0px"}}>
+        <Row style={{ margin: "100px 0px 50px 0px" }}>
           <Col md="4">
-              <a href="https://www.sadiephoto.com/" style={{width: "100% !important"}}>
-                <img src="Capture.PNG" style={{ margin: "auto", width: "100%", border: "1px solid grey"}}>
-                </img>
-                <h5 className="text-center pt-3">Sadie Photo</h5>
-              </a>
+            <a
+              href="https://www.sadiephoto.com/"
+              style={{ width: "100% !important" }}
+              className="float-card"
+            >
+              <img
+                src="Capture.PNG"
+                style={{
+                  margin: "auto",
+                  width: "100%",
+                  boxShadow: "20px 20px 50px #bebebe, -20px -20px 50px #ffffff",
+                  borderRadius: "15px",
+                }}
+              ></img>
+              <h5 className="text-center pt-3 gradient">Sadie Photo</h5>
+            </a>
           </Col>
           <Col md="4">
-              <a href="https://www.decheque.com/" style={{width: "100%"}}>
-                <img src="Capture2.PNG" style={{margin: "auto",width: "100%", border: "1px solid grey"}}>
-                </img>
-                <h5 className="text-center pt-3">Decheque</h5>
-              </a>
+            <a
+              href="https://www.velvetfs.com/"
+              style={{ width: "100%" }}
+              className="float-card"
+            >
+              <img
+                src="velvet.PNG"
+                style={{
+                  margin: "auto",
+                  width: "100%",
+                  boxShadow: "20px 20px 50px #bebebe, -20px -20px 50px #ffffff",
+                  borderRadius: "15px",
+                }}
+              ></img>
+              <h5 className="text-center pt-3 gradient">Velvet</h5>
+            </a>
           </Col>
           <Col md="4">
-              <a href="https://worldclassintegration.vercel.app/" style={{width: "100%"}}>
-                <img src="Capture3.PNG" style={{ margin: "auto", width: "100%", border: "1px solid grey"}}>
-                </img>
-                <h5 className="text-center pt-3">World Class Integration</h5>
-              </a>
+            <a
+              href="https://worldclassintegration.vercel.app/"
+              style={{ width: "100%" }}
+              className="float-card"
+            >
+              <img
+                src="Capture3.PNG"
+                style={{
+                  margin: "auto",
+                  width: "100%",
+                  boxShadow: "20px 20px 50px #bebebe, -20px -20px 50px #ffffff",
+                  borderRadius: "15px",
+                }}
+              ></img>
+              <h5 className="text-center pt-3 gradient">
+                World Class Integration
+              </h5>
+            </a>
           </Col>
         </Row>
-        <Row style={{margin: "0px 0px 100px 0px"}}>
+        <Row style={{ margin: "0px 0px 100px 0px" }}>
           <Col md="4">
-                <a href="/algorithms" style={{width: "100%"}}>
-                  <img src="Capture4.PNG" style={{ margin: "auto", width: "100%", border: "1px solid grey"}}>
-                  </img>
-                  <h5 className="text-center pt-3">Algos and DS Visualized</h5>
-                </a>
-            </Col>
+            <a
+              href="/algorithms"
+              style={{ width: "100%" }}
+              className="float-card"
+            >
+              <img
+                src="Capture4.PNG"
+                style={{
+                  margin: "auto",
+                  width: "100%",
+                  boxShadow: "20px 20px 50px #bebebe, -20px -20px 50px #ffffff",
+                  borderRadius: "15px",
+                }}
+              ></img>
+              <h5 className="text-center pt-3 gradient">
+                Data Structures and Algorithms Visualized
+              </h5>
+            </a>
+          </Col>
         </Row>
       </Container>
     </>
-  )
+  );
 }
