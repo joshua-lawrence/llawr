a8ad8ce5cdb4421059902775b38d8f3e2266d7cf
Joshua Lawrence <joshua98lawrence@gmail.com>
2023-04-17 16:17:57 -0600

    oops

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,68 +10,74 @@ export default function Home() {
         <title>Joshua Lawrence</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Navbar style={{}}></Navbar> */}
       <div className="color-shadow" style={{}}>
-        <Container lg responsive={false}>
-          <Grid.Container>
-            <Grid
-              md="8"
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
-            >
-              <div style={{ padding: 36 }}>
-                <Text
-                  h1
-                  css={{
-                    textGradient: "45deg, $blue600 -20%, $pink600 70%",
-                    margin: 0,
-                    padding: 0,
-                  }}
-                >
-                  Joshua Lawrence
-                </Text>
-                <Text
-                  h3
-                  color="white"
-                  size={28}
-                  weight={300}
-                  css={{ marginTop: 0, marginBottom: 8, padding: 0 }}
-                >
-                  CTO at Velvet
-                </Text>
-                <Text h4 color="white" weight="thin" css={{ marginBottom: 8 }}>
-                  Engineering · DevOps · AI · Data Driven Decision Making
-                </Text>
-                <Grid.Container>
-                  <Grid>
-                    <a href="https://github.com/joshua-lawrence">
-                      <AiFillGithub fontSize={36} />
-                    </a>
-                  </Grid>
-                  <Grid>
-                    <a href="https://www.linkedin.com/in/joshuaglawrence/">
-                      <AiFillLinkedin fontSize={36} />
-                    </a>
-                  </Grid>
-                </Grid.Container>
-              </div>
-            </Grid>
-            <Grid md="4">
-              <img
-                className="me"
+        <div style={{ backgroundColor: "black", borderRadius: "40px" }}>
+          <Container lg responsive={false}>
+            <Grid.Container>
+              <Grid
+                md="8"
                 style={{
-                  width: "70%",
-                  paddingTop: "50px",
-                  marginLeft: "100px",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
                 }}
-                src="me (1).png"
-              ></img>
-            </Grid>
-          </Grid.Container>
-        </Container>
+              >
+                <div className="body-card" style={{ padding: 36 }}>
+                  <Text
+                    h1
+                    css={{
+                      textGradient: "45deg, $blue600 -20%, $pink600 70%",
+                      margin: 0,
+                      padding: 0,
+                    }}
+                  >
+                    Joshua Lawrence
+                  </Text>
+                  <Text
+                    h3
+                    color="white"
+                    size={28}
+                    weight={300}
+                    css={{ marginTop: 0, marginBottom: 8, padding: 0 }}
+                  >
+                    CTO at Velvet
+                  </Text>
+                  <Text
+                    h4
+                    color="white"
+                    weight="thin"
+                    css={{ marginBottom: 8 }}
+                  >
+                    Engineering · DevOps · AI · Data Driven Decision Making
+                  </Text>
+                  <Grid.Container>
+                    <Grid>
+                      <a href="https://github.com/joshua-lawrence">
+                        <AiFillGithub fontSize={36} />
+                      </a>
+                    </Grid>
+                    <Grid>
+                      <a href="https://www.linkedin.com/in/joshuaglawrence/">
+                        <AiFillLinkedin fontSize={36} />
+                      </a>
+                    </Grid>
+                  </Grid.Container>
+                </div>
+              </Grid>
+              <Grid md="4">
+                <img
+                  className="me"
+                  style={{
+                    width: "70%",
+                    paddingTop: "50px",
+                    marginLeft: "100px",
+                  }}
+                  src="me (1).png"
+                ></img>
+              </Grid>
+            </Grid.Container>
+          </Container>
+        </div>
       </div>
     </>
   );
